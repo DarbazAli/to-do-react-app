066d8dcb82ca2b9992178a3c32e0b6386003e878
Darbaz Ali <darbaz.a.h@gmail.com>
2020-08-08 08:54:16 +0300

    redefine

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { render } from "react-dom";
 
-
+const Header = ({ lenght }) => {
+  return <h4>You have {length} Todos</h4>;
+};
 const App = () => {
+  const [todos, setTodos] = useState([]);
   return (
     <div>
-      <h1>Hello React!</h1>
+      <Header lenght={todos.lenght} />
     </div>
   );
 };
