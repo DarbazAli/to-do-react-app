8d935034f186ede62444068307211685ef5621d6
Darbaz Ali <darbaz.a.h@gmail.com>
2020-08-08 09:00:53 +0300

    create ToDos

--- src/App.js
+++ src/App.js
@@ -4,6 +4,18 @@ import { render } from "react-dom";
 const Header = ({ lenght }) => {
   return <h4>You have {length} Todos</h4>;
 };
+
+const ToDos = ({ items }) => {
+  return items.lenght === 0 ? (
+    <p>There is no todo for now!</p>
+  ) : (
+    <div>
+      {items.map((item, index) => (
+        <Todo key={index} item={item} />
+      ))}
+    </div>
+  );
+};
 const App = () => {
   const [todos, setTodos] = useState([]);
   return (
