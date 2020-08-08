665e1dc5705123a3d989ee3d6cb04d7ca8366d59
Darbaz Ali <darbaz.a.h@gmail.com>
2020-08-08 09:26:51 +0300

    create Form

--- src/App.js
+++ src/App.js
@@ -1,44 +1,56 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { render } from "react-dom";
 
 const Header = ({ lenght }) => {
   return <h4>You have {length} Todos</h4>;
 };
 
-const ToDo = ({item}) => {
-  console.log(item);
+const ToDo = ({ item }) => {
   return (
     <div>
       <p>{item}</p>
       <button>X</button>
     </div>
-  )
-}
+  );
+};
 const ToDos = ({ items }) => {
-  if ( items.lenght < 1 ) {
-    return (<p>There is no todo for now</p>)
+  if (items.lenght < 1) {
+    return <p>There is no todo for now</p>;
   } else {
     return (
       <div>
-        {
-          items.map( (item, index) => {
-            return (<ToDo key={index} item={item} />)
-          })
-        }
+        {items.map((item, index) => {
+          return <ToDo key={index} item={item} />;
+        })}
       </div>
-    )
+    );
   }
-}
-
+};
 
 const App = () => {
-  const [todos, setTodos] = useState(['Read a book']);
+  const [todos, setTodos] = useState([]);
+  const [todo, setTodo] = useState("");
+
+  useEffect(() => {}, [todos]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setTodo("");
+    todos.push(todo);
+  };
   return (
     <div>
       <Header lenght={todos.lenght} />
-      <ToDos 
-        items={todos}
-      />
+      <ToDos items={todos} />
+      <form onSubmit={handleSubmit}>
+        <input
+          value={todo}
+          onChange={(e) => setTodo(e.target.value)}
+          placeholder="Enter todo"
+          type="text"
+        ></input>
+        <button>Submit</button>
+      </form>
     </div>
   );
 };
