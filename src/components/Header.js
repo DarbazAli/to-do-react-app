dcde4f9d08a3f55481c97e562e3d231fe4ed119e
Darbaz Ali <darbaz.a.h@gmail.com>
2020-08-08 10:04:59 +0300

    install prop-types

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types';
 
 const Header = ({length}) => {
     return (
@@ -6,6 +7,10 @@ const Header = ({length}) => {
             <p>You have <b>{length}</b> todos</p>
         </div>
     )
+}
+
+Header.propTypes = {
+    length: PropTypes.number
 }
 
 export default Header
