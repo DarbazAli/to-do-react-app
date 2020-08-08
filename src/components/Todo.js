3c4c6e44cd2277e772cbc18230f41ad2fc64cbef
Darbaz Ali <darbaz.a.h@gmail.com>
2020-08-08 10:12:27 +0300

    create Todo

---
+++ src/components/Todo.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import PropTypes from 'prop-types';
+
+const Todo = ({item, onDelete, id}) => {
+    return (
+        <div className="list-item">
+            <h3>{item}</h3>
+            <button onClick={onDelete(id)}>Delete</button>
+        </div>
+    )
+}
+
+Todo.propTypes = {
+    item: PropTypes.string,
+    onDelete: PropTypes.func,
+    id: PropTypes.number
+}
+export default Todo
