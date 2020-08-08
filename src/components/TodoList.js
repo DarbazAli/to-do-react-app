fae045eef5d8232757a87da05680735a0e0119e1
Darbaz Ali <darbaz.a.h@gmail.com>
2020-08-08 10:19:32 +0300

    create TodoList

---
+++ src/components/TodoList.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import PropTypes from 'prop-types';
+import Todo from './Todo'
+
+const TodoList = ({onDelete, todos}) => {
+    return (
+        <div className="todo-list">
+            {
+                todos.map((item, index) => {
+                    return <Todo 
+                        key={index}
+                        item={item}
+                        onDelete={onDelete}
+                    />
+                })
+            }
+        </div>
+    )
+}
+
+TodoList.propTypes = {
+    onDelete: PropTypes.func,
+    todos: PropTypes.array
+}
+export default TodoList
